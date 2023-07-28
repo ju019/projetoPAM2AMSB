80fa96b9ecd3d972de001e57f15d7f5360b590d2
ju019 <140768491+ju019@users.noreply.github.com>
2023-07-27 21:40:46 -0300

    Add files via upload

---
+++ AMS_TRIANGULO/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Digite o número :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite um número para o tamanho do triângulo :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("");
+int j, i;
+for ( j = 0; j < num2; j++)
+{
+  for ( i = j; i < num2; i++)
+  {
+    
+       Console.Write(num1);
+       
+    
+    }
+    Console.WriteLine("");
+  }
+  
+Console.WriteLine($" número :{num1} , repetido : {num2}");
+
+
+
+
